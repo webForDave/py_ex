3497533b5dad9b0aeec6483e1b6d40effa998413
David Akinola <dakinola54@gmail.com>
2024-10-29 10:39:33 +0100

    Highets and lowest numbers

---
+++ highest_and_lowest/high_and_low.py
@@ -0,0 +1,15 @@
+def main():
+    print(high_and_low('1 2 -3 4 5'))
+
+
+def high_and_low(numbers):
+    numbers = numbers.split(' ')
+    numbers_list = []
+    for i in numbers:
+        numbers_list.append(i)
+        max_and_min = [max(numbers_list), min(numbers_list)]
+    return ' '.join(max_and_min)
+
+
+if __name__ == "__main__":
+    main()
